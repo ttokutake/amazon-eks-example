61e6cec1a01cb749faeda2c1f5556c6d7b7cccb5
ttokutake <tadatoshi.tokutake@gmail.com>
2023-07-04 21:34:01 +0900

    Add subnets

--- locals.tf
+++ locals.tf
@@ -1,3 +1,6 @@
 locals {
-  region = "ap-northeast-1"
+  region              = "ap-northeast-1"
+  availability_zone_1 = "ap-northeast-1a"
+  availability_zone_2 = "ap-northeast-1c"
+  availability_zone_3 = "ap-northeast-1d"
 }
--- main.tf
+++ main.tf
@@ -20,3 +20,63 @@ resource "aws_vpc" "main" {
     Name = "eks_example"
   }
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_1
+  cidr_block        = "192.168.0.0/19"
+
+  tags = {
+    Name = "eks_example_public_1"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_1
+  cidr_block        = "192.168.32.0/19"
+
+  tags = {
+    Name = "eks_example_private_1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_2
+  cidr_block        = "192.168.64.0/19"
+
+  tags = {
+    Name = "eks_example_public_2"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_2
+  cidr_block        = "192.168.96.0/19"
+
+  tags = {
+    Name = "eks_example_private_2"
+  }
+}
+
+resource "aws_subnet" "public_3" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_3
+  cidr_block        = "192.168.128.0/19"
+
+  tags = {
+    Name = "eks_example_public_3"
+  }
+}
+
+resource "aws_subnet" "private_3" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.availability_zone_3
+  cidr_block        = "192.168.160.0/19"
+
+  tags = {
+    Name = "eks_example_private_3"
+  }
+}
