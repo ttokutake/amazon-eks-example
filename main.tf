d79e28dddd3bd5ced3f4bf9d3f4da23b3c040bef
ttokutake <tadatoshi.tokutake@gmail.com>
2023-07-07 00:28:55 +0900

    Add Route Table for public subnets

--- main.tf
+++ main.tf
@@ -80,6 +80,34 @@ resource "aws_nat_gateway" "private_3" {
   depends_on = [aws_internet_gateway.main]
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "eks_example_public"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_3" {
+  subnet_id      = aws_subnet.public_3.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   availability_zone = local.availability_zone_1
