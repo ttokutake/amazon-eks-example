1aa36d9b12669c25eccd26044b7cda24dcd62621
ttokutake <tadatoshi.tokutake@gmail.com>
2023-07-07 17:06:47 +0900

    Fix gateway_id => nat_gateway_id

--- vpc.tf
+++ vpc.tf
@@ -157,8 +157,8 @@ resource "aws_route_table" "private_1" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_1.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_1.id
   }
 
   tags = {
@@ -170,8 +170,8 @@ resource "aws_route_table" "private_2" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_2.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_2.id
   }
 
   tags = {
@@ -183,8 +183,8 @@ resource "aws_route_table" "private_3" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_3.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.private_3.id
   }
 
   tags = {
