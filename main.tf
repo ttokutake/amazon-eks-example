b2580d087af27181a2173040c63f2f4ab7c9ec95
ttokutake <tadatoshi.tokutake@gmail.com>
2023-07-07 16:36:37 +0900

    Add IAM Role for cluster

--- main.tf
+++ main.tf
@@ -12,3 +12,14 @@ terraform {
 provider "aws" {
   region = local.region
 }
+
+resource "aws_iam_role" "main" {
+  name = "AmazonEKSExampleClusterRole"
+
+  assume_role_policy = file("./policies/eks-cluster-role-trust-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = aws_iam_role.main.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
