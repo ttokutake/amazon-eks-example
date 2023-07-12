a61e0ee7f86bdb514c62173ace77e3cb786b56a7
ttokutake <tadatoshi.tokutake@gmail.com>
2023-07-12 22:31:14 +0900

    Create OpenID Connect Provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,3 +87,15 @@ resource "aws_eks_fargate_profile" "example" {
     namespace = "eks-sample-app"
   }
 }
+
+data "tls_certificate" "main" {
+  url = aws_eks_cluster.main.identity.0.oidc.0.issuer
+}
+
+resource "aws_iam_openid_connect_provider" "main" {
+  url = aws_eks_cluster.main.identity.0.oidc.0.issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [data.tls_certificate.main.certificates.0.sha1_fingerprint]
+}
